52e95efb7301dd87c77709fac1dc1d3b19df5f6b
DoMinhHieu <dohieu19999@gmail.com>
2021-03-18 08:59:21 +0700

    1803 hieudm

---
+++ app/src/main/java/com/dohieu19999/firebasechat/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.dohieu19999.firebasechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
